571affbf2d308db21b2846ac3f06d8781f84e316
analuciachin <ananime@gmail.com>
2021-02-02 16:24:04 +0000

    work on exercise 7 - validation for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+end
+
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than_or_equal_to: 200 }
+  validates :belongs_to, presence: true
+end
+
+puts "Enter a store name"
+store_name = $stdin.gets.chomp
+create_store = Store.create(name: store_name)
+puts create_store.valid?
+puts create_store.errors[:annual_revenue]
